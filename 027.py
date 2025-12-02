da2e4fe3cf4dfd4f628ee860b95521705e73bffb
1vanK <ivan.kozyrev@edunord.se>
2025-12-02 12:15:53 +0100

    .

---
+++ 027.py
@@ -0,0 +1,12 @@
+psn = " August Strindberg "   
+
+print("\t August Strindberg")
+
+#print("\t "August\n\tStrindberg")
+
+psn.rstrip()
+print (psn)
+psn.lstrip()
+print (psn)
+psn.strip()
+print (psn)
