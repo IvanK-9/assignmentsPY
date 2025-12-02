b54739be3212bc20230ca3f7691df88e8ab8d82b
1vanK <ivan.kozyrev@edunord.se>
2025-12-02 10:36:24 +0100

    some fixes

---
+++ 25.py
@@ -0,0 +1,2 @@
+# vvv
+print('August Strindberg once said, "Life is not so idiotically mathematical that only the big eat the small; it is just as common for a bee to kill a lion or at least to drive it mad."')
