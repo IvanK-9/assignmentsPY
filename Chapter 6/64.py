3f867e987f80b11b52ce89d815e346fc83d2ca5c
1vanK <ivan.kozyrev@edunord.se>
2025-12-12 14:14:37 +0100

    z

---
+++ Chapter 6/64.py
@@ -0,0 +1,12 @@
+cities = {
+    'Paris': {
+        'country': 'France',
+        'population': 2161000,
+        'fact': 'Home of the Eiffel Tower'
+    },
+    'Tokyo': {
+        'country': 'Japan',
+        'population': 13960000,
+        'fact': 'Largest metropolitan area in the world'
+    },
+    
